e68713dadcd8b47fa2284b8e333de299ebc6d729
marco <marco@webguerilla.net>
2019-01-22 23:17:55 +0100

    formatted source

--- jquery.unveil.js
+++ jquery.unveil.js
@@ -9,48 +9,50 @@
  */
 
 ;(function($) {
+	$.fn.unveil = function(threshold, callback) {
 
-  $.fn.unveil = function(threshold, callback) {
+	var $w = $(window),
+		th = threshold || 0,
+		retina = window.devicePixelRatio > 1,
+		attrib = retina? "data-src-retina" : "data-src",
+		images = this,
+		loaded;
 
-    var $w = $(window),
-        th = threshold || 0,
-        retina = window.devicePixelRatio > 1,
-        attrib = retina? "data-src-retina" : "data-src",
-        images = this,
-        loaded;
+	this.one("unveil", function() {
+		var source = this.getAttribute(attrib);
+		source = source || this.getAttribute("data-src");
+		if (source) {
+			this.setAttribute("src", source);
+			if (typeof callback === "function") {
+				callback.call(this);
+			}
+		}
+	});
 
-    this.one("unveil", function() {
-      var source = this.getAttribute(attrib);
-      source = source || this.getAttribute("data-src");
-      if (source) {
-        this.setAttribute("src", source);
-        if (typeof callback === "function") callback.call(this);
-      }
-    });
+	function unveil() {
+		var inview = images.filter(function() {
+			var $e = $(this);
+			if ($e.is(":hidden")) {
+				return;
+			}
 
-    function unveil() {
-      var inview = images.filter(function() {
-        var $e = $(this);
-        if ($e.is(":hidden")) return;
+			var wt = $w.scrollTop(),
+				wb = wt + $w.height(),
+				et = $e.offset().top,
+				eb = et + $e.height();
 
-        var wt = $w.scrollTop(),
-            wb = wt + $w.height(),
-            et = $e.offset().top,
-            eb = et + $e.height();
+			return eb >= wt - th && et <= wb + th;
+		});
 
-        return eb >= wt - th && et <= wb + th;
-      });
+		loaded = inview.trigger("unveil");
+		images = images.not(loaded);
+	}
 
-      loaded = inview.trigger("unveil");
-      images = images.not(loaded);
-    }
+	$w.on("scroll.unveil resize.unveil lookup.unveil", unveil);
 
-    $w.on("scroll.unveil resize.unveil lookup.unveil", unveil);
+	unveil();
 
-    unveil();
-
-    return this;
-
-  };
+	return this;
+};
 
 })(window.jQuery || window.Zepto);
